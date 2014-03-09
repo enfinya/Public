3c6d6c729805e5184320d826fd4b7c268db579ab
enfinya <bons13@gmail.com>
2014-03-09 13:45:04 +0100

    Primer proyecto
    Primera pre-alfa

---
+++ proyecto/Assets/Scripts/MoveScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Simply moves the current game object
+/// </summary>
+public class MoveScript : MonoBehaviour
+{
+	// 1 - Designer variables
+	
+	/// <summary>
+	/// Object speed
+	/// </summary>
+	public Vector2 speed = new Vector2(10, 10);
+	
+	/// <summary>
+	/// Moving direction
+	/// </summary>
+	public Vector2 direction = new Vector2(-1, 0);
+	
+	private Vector2 movement;
+	
+	void Update()
+	{
+		// 2 - Movement
+		movement = new Vector2(
+			speed.x * direction.x,
+			speed.y * direction.y);
+	}
+	
+	void FixedUpdate()
+	{
+		// Apply movement to the rigidbody
+		rigidbody2D.velocity = movement;
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		bool damagePlayer = false;
+		
+		// Collision with enemy
+		EnemyScript enemy = collision.gameObject.GetComponent<EnemyScript>();
+		if (enemy != null)
+		{
+			// Kill the enemy
+			HealthScript enemyHealth = enemy.GetComponent<HealthScript>();
+			if (enemyHealth != null) enemyHealth.Damage(enemyHealth.hp);
+			
+			damagePlayer = true;
+		}
+		
+		// Damage the player
+		if (damagePlayer)
+		{
+			HealthScript playerHealth = this.GetComponent<HealthScript>();
+			if (playerHealth != null) playerHealth.Damage(1);
+		}
+	}
+}
